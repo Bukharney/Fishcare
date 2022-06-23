04ac5df6845a41b85baea54fb225c5f6e175009a
Jirapat <bukharney@gmail.com>
2022-06-23 16:08:04 +0700

    23-6

--- lib/views/display_view.dart
+++ lib/views/display_view.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fish_care/widget/day.dart';
+import 'package:fish_care/widget/lamp.dart';
 import 'package:fish_care/widget/setlamp.dart';
 import 'package:fish_care/widget/settemp.dart';
 import 'package:fish_care/widget/temp.dart';
@@ -24,7 +25,7 @@ class _DisplayViewState extends State<DisplayView> {
   late Timer _timer;
 
   List<int> date = <int>[0, 0];
-  List<int> lamp = <int>[0, 0, 0, 0];
+  List<int> lamp = <int>[0, 0, 0, 0, 0, 0];
   List<double> temp = <double>[0.0, 0.0, 0.0];
   var now = DateTime.now();
   late TimeOfDay timeOn;
@@ -53,6 +54,8 @@ class _DisplayViewState extends State<DisplayView> {
         lamp[1] = event.snapshot.child('lamp/onM').value as int;
         lamp[2] = event.snapshot.child('lamp/offH').value as int;
         lamp[3] = event.snapshot.child('lamp/offM').value as int;
+        lamp[4] = event.snapshot.child('lamp/status').value as int;
+        lamp[5] = event.snapshot.child('lamp/mode').value as int;
         temp[0] = event.snapshot.child('temp/low').value as double;
         temp[1] = event.snapshot.child('temp/high').value as double;
         temp[2] = event.snapshot.child('temp/now').value as double;
@@ -79,6 +82,10 @@ class _DisplayViewState extends State<DisplayView> {
                         height: 15,
                       ),
                       Temp(temp: temp),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Lamp(lamp: lamp),
                     ],
                   ),
                 ),
--- lib/widget/lamp.dart
+++ lib/widget/lamp.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:fish_care/utilities/show_error_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class Lamp extends StatefulWidget {
-  const Lamp({Key? key}) : super(key: key);
+  List lamp;
+  Lamp({Key? key, required this.lamp}) : super(key: key);
 
   @override
-  State<Lamp> createState() => _LampState();
+  State<Lamp> createState() => _LampState(lamp);
 }
 
 class _LampState extends State<Lamp> {
+  List lamp;
+  _LampState(this.lamp);
+  final _database = FirebaseDatabase.instance.ref();
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -16,26 +23,26 @@ class _LampState extends State<Lamp> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
-          padding: EdgeInsets.all(8.0),
-          width: 160,
+          padding: const EdgeInsets.all(8.0),
+          width: 330,
           height: 120,
           decoration: BoxDecoration(
               color: Colors.white, borderRadius: BorderRadius.circular(16)),
           child: Column(
             children: [
-              Text(
-                name[0],
+              const Text(
+                'LAMP',
                 style: TextStyle(fontWeight: FontWeight.w600),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               ToggleSwitch(
                 changeOnTap: false,
-                animationDuration: 300,
-                minWidth: 60,
+                animationDuration: 100,
+                minWidth: 100,
                 minHeight: 40.0,
-                initialLabelIndex: db[0],
+                initialLabelIndex: lamp[4],
                 cornerRadius: 20.0,
                 activeFgColor: Colors.white,
                 inactiveBgColor: Colors.grey,
@@ -53,38 +60,30 @@ class _LampState extends State<Lamp> {
                 animate: true,
                 curve: Curves.bounceInOut,
                 onToggle: (value) async {
-                  try {
-                    if (db[2] == 1) {
-                      context.read<LoadingProvider>().setLoad(true);
-                      await wdb.update({name[0]: value});
-                      context.read<LoadingProvider>().setLoad(false);
-                    } else {
-                      Fluttertoast.showToast(
-                          msg: 'Please select manual mode!',
-                          gravity: ToastGravity.CENTER);
-                    }
-                  } catch (e) {
-                    print(e);
+                  if (lamp[5] == 0) {
+                    showErrorDialog(context, 'Plaese select manual first!');
+                  } else {
+                    await _database.update({'data/lamp/status/': value});
                   }
                 },
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               ToggleSwitch(
                 changeOnTap: false,
                 animationDuration: 300,
                 radiusStyle: true,
-                minWidth: 60,
+                minWidth: 80,
                 minHeight: 20.0,
-                initialLabelIndex: db[2],
+                initialLabelIndex: lamp[5],
                 cornerRadius: 20.0,
                 activeFgColor: Colors.white,
                 inactiveBgColor: Colors.grey,
                 inactiveFgColor: Colors.white,
                 activeBgColors: const [
-                  [Colors.green, Colors.amberAccent],
-                  [Colors.green, Colors.amberAccent],
+                  [Colors.green, Color.fromARGB(255, 226, 255, 64)],
+                  [Color.fromARGB(255, 226, 255, 64), Colors.green],
                 ],
                 totalSwitches: 2,
                 labels: const ['AUTO', 'MANUAL'],
@@ -93,106 +92,7 @@ class _LampState extends State<Lamp> {
                 animate: true,
                 curve: Curves.bounceInOut,
                 onToggle: (value) async {
-                  try {
-                    context.read<LoadingProvider>().setLoad(true);
-                    await wdb.update({name[2]: value});
-                    context.read<LoadingProvider>().setLoad(false);
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Container(
-          padding: EdgeInsets.all(8.0),
-          width: 160,
-          height: 120,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(16)),
-          child: Column(
-            children: [
-              Text(
-                name[1],
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ToggleSwitch(
-                changeOnTap: false,
-                animationDuration: 300,
-                minWidth: 60,
-                minHeight: 40.0,
-                initialLabelIndex: db[1],
-                cornerRadius: 20.0,
-                activeFgColor: Colors.white,
-                inactiveBgColor: Colors.grey,
-                inactiveFgColor: Colors.white,
-                icons: const [
-                  Icons.flash_off_rounded,
-                  Icons.flash_on_rounded,
-                ],
-                totalSwitches: 2,
-                iconSize: 30.0,
-                activeBgColors: const [
-                  [Colors.black45, Colors.black26],
-                  [Colors.yellow, Colors.orange]
-                ],
-                animate: true,
-                curve: Curves.bounceInOut,
-                onToggle: (value) async {
-                  try {
-                    if (db[3] == 1) {
-                      context.read<LoadingProvider>().setLoad(true);
-                      await wdb.update({name[1]: value});
-                      context.read<LoadingProvider>().setLoad(false);
-                    } else {
-                      Fluttertoast.showToast(
-                          msg: 'Please select manual mode!',
-                          gravity: ToastGravity.CENTER);
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ToggleSwitch(
-                changeOnTap: false,
-                animationDuration: 300,
-                radiusStyle: true,
-                minWidth: 60,
-                minHeight: 20.0,
-                initialLabelIndex: db[3],
-                cornerRadius: 20.0,
-                activeFgColor: Colors.white,
-                inactiveBgColor: Colors.grey,
-                inactiveFgColor: Colors.white,
-                activeBgColors: const [
-                  [Colors.green, Colors.amberAccent],
-                  [Colors.green, Colors.amberAccent],
-                ],
-                totalSwitches: 2,
-                labels: const ['AUTO', 'MANUAL'],
-                fontSize: 8,
-                iconSize: 30.0,
-                animate: true,
-                curve: Curves.bounceInOut,
-                onToggle: (value) async {
-                  try {
-                    context.read<LoadingProvider>().setLoad(true);
-                    await wdb.update({name[3]: value});
-                    context.read<LoadingProvider>().setLoad(false);
-                  } catch (e) {
-                    print(e);
-                  }
+                  await _database.update({'data/lamp/mode/': value});
                 },
               ),
             ],
